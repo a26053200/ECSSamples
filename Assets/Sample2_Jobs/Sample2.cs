67925b1e4e7b5692f55f5d77e2bc83e80c31fd9a
a26053200 <zhengnan262@qq.com>
2019-09-01 12:52:55 +0800

    ECSSample

--- Assets/Sample2_Jobs/Sample2.cs
+++ Assets/Sample2_Jobs/Sample2.cs
@@ -25,9 +25,11 @@ public class Sample2 : MonoBehaviour
 
     private List<Dragon> dragonList;
 
+    private float speed = 1f;
+
     public class Dragon
     {
-        public Transform dragon;
+        public Transform transform;
         public float moveY;
     }
 
@@ -41,15 +43,17 @@ public class Sample2 : MonoBehaviour
             Transform dragon = Instantiate(dragonPrefab, new Vector3(Random.Range(-8f, 8f), Random.Range(-5f, 5f), 0), Quaternion.identity);
             dragonList.Add(new Dragon
             {
-                dragon = dragon,
-                moveY = Random.Range(-5f, 5f)
+                transform = dragon,
+                moveY = Random.Range(1f, 5f)
             });
         }
+
+        
     }
 
     void Update()
     {
-        float startTime = Time.realtimeSinceStartup;
+        //float startTime = Time.realtimeSinceStartup;
         //if (useJobs)
         //{
         //    NativeList<JobHandle> jobHandles = new NativeList<JobHandle>(Allocator.Temp);
@@ -68,7 +72,55 @@ public class Sample2 : MonoBehaviour
         //        ReallyTouchTask();
         //    }
         //}
-        Debug.Log((Time.realtimeSinceStartup - startTime) * 1000f + "ms");
+        if (useJobs)
+        {
+            NativeArray<float3> positions = new NativeArray<float3>(dragonList.Count, Allocator.TempJob);
+            NativeArray<float> moveYs = new NativeArray<float>(dragonList.Count, Allocator.TempJob);
+
+            for (int i = 0; i < dragonList.Count; i++)
+            {
+                positions[i] = dragonList[i].transform.position;
+                moveYs[i] = dragonList[i].moveY;
+            }
+            ReallyTouchJobParallelFor reallyTouchJobParallelFor = new ReallyTouchJobParallelFor
+            {
+                deltaTime = Time.deltaTime,
+                positions = positions,
+                moveYs = moveYs,
+            };
+            reallyTouchJobParallelFor.Schedule(dragonList.Count, 100).Complete();
+
+            for (int i = 0; i < dragonList.Count; i++)
+            {
+                dragonList[i].transform.position = positions[i];
+                dragonList[i].moveY = moveYs[i];
+            }
+
+            positions.Dispose();
+            moveYs.Dispose();
+        }
+        else
+        {
+            for (int i = 0; i < dragonList.Count; i++)
+            {
+                Dragon dragon = dragonList[i];
+                Vector3 pos = dragon.transform.position;
+                pos += new Vector3(0, dragon.moveY * Time.deltaTime, 0);
+                if (pos.y > 5)
+                    dragon.moveY = -math.abs(dragon.moveY);
+                if (pos.y < -5)
+                    dragon.moveY = +math.abs(dragon.moveY);
+                dragonList[i].transform.position = pos;
+                //莫妮卡
+                float value = 0f;
+                for (int j = 0; j < 1000; j++)
+                {
+                    value = math.exp10(math.sqrt(value));
+                }
+            }
+        }
+        
+        //Debug.Log((Time.realtimeSinceStartup - startTime) * 1000f + "ms");
     }
 
     private void ReallyTouchTask()
@@ -94,6 +146,29 @@ public struct ReallyTouchJob : IJob
     {
         float value = 0f;
         for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
+
+[BurstCompile]
+public struct ReallyTouchJobParallelFor : IJobParallelFor
+{
+    public NativeArray<float3> positions;
+    public NativeArray<float> moveYs;
+    [ReadOnly] public float deltaTime;
+
+    public void Execute(int i)
+    {
+        positions[i] += new float3(0,moveYs[i] * deltaTime, 0);
+        if (positions[i].y > 5)
+            moveYs[i] = -math.abs(moveYs[i]);
+        if (positions[i].y < -5)
+            moveYs[i] = +math.abs(moveYs[i]);
+        //莫妮卡
+        float value = 0f;
+        for (int j = 0; j < 1000; j++)
         {
             value = math.exp10(math.sqrt(value));
         }
